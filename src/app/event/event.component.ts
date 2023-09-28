17bdc8a0523363ced974842b19b8140ddf311420
jhonatansanga58 <sbj2019949@est.univalle.edu>
2023-09-28 12:10:21 -0400

    feature/create-event

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {MatDialog, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { ModalCancelRegistrationComponent } from '../modal-cancel-registration/modal-cancel-registration.component';
 import { Router } from '@angular/router';
+import { ModalUpdateEventComponent } from '../modal-update-event/modal-update-event.component';
 
 @Component({
   selector: 'app-event',
@@ -13,11 +14,19 @@ export class EventComponent {
   constructor(
     public dialog: MatDialog,
     private router: Router
-    ){}
+  ) { }
 
-  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.dialog.open(ModalCancelRegistrationComponent, {
-      width: '250px',
+  openCancelRegistrationDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.openDialog(enterAnimationDuration, exitAnimationDuration, ModalCancelRegistrationComponent);
+  }
+
+  openEditEventDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.openDialog(enterAnimationDuration, exitAnimationDuration, ModalUpdateEventComponent);
+  }
+
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string, modal: any): void {
+    this.dialog.open(modal, {
+      width: 'auto',
       enterAnimationDuration,
       exitAnimationDuration,
     });
@@ -25,9 +34,5 @@ export class EventComponent {
   
   public eventInfo() {
     this.router.navigate(['/event', 0]);
-  }
-
-  public editEvent() {
-    this.router.navigate(['/edit', 0]);
   }
 }
