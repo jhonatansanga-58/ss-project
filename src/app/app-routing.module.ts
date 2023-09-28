e632f631674100f944d1c914805bb4071777eb75
jhonatansanga58 <sbj2019949@est.univalle.edu>
2023-09-27 21:02:43 -0400

    feature/event-info-view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { MyEventsComponent } from './my-events/my-events.component';
 import { EventsComponent } from './events/events.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { LoginComponent } from './login/login.component';
+import { EventInfoComponent } from './event-info/event-info.component';
 
 const routes: Routes = [
   {path: '', component: EventsComponent},
   {path: 'myevents', component: MyEventsComponent},
   {path: 'myprofile', component: MyProfileComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'event/:id', component: EventInfoComponent}
 ];
 
 @NgModule({
--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatDialog, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
 import { ModalCancelRegistrationComponent } from '../modal-cancel-registration/modal-cancel-registration.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event',
@@ -8,12 +9,21 @@ import { ModalCancelRegistrationComponent } from '../modal-cancel-registration/m
   styleUrls: ['./event.component.scss']
 })
 export class EventComponent {
-  constructor(public dialog: MatDialog){}
+
+  constructor(
+    public dialog: MatDialog,
+    private router: Router
+    ){}
+
   openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
     this.dialog.open(ModalCancelRegistrationComponent, {
       width: '250px',
       enterAnimationDuration,
       exitAnimationDuration,
     });
+  }
+  
+  public eventInfo() {
+    this.router.navigate(['/event', 0]);
   }
 }
