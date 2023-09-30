950f69d2a9917b8db61ac826c6dee9837bbbd3a6
jhonatansanga58 <sbj2019949@est.univalle.edu>
2023-09-30 05:17:25 -0400

    feature/list-events-functionality

--- src/app/services/events/events.service.ts
+++ src/app/services/events/events.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getEvents() {
+    return this.http.get(`${environment.apiUrl}/event`);
+  }
+
+  public getUnregisteredEvents(id) {
+    return this.http.get(`${environment.apiUrl}/event/unregistered/` + id);
+  }
+
+  public getRegisteredEvents(id) {
+    return this.http.get(`${environment.apiUrl}/event/registered/` + id);
+  }
 }
